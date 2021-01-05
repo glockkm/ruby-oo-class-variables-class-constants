d6d66bd576565f6c7ca6de6940294e94fb122817
Kimberly <kmglock@mail.lipscomb.edu>
2021-01-04 21:23:43 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,10 @@
+
+
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count  # remove the attr_accessor for genre because customizing below
+  attr_reader :title, :genre  # add an attr_reader for genre because need a reader/getter
+
+  GENRES = [] # class constant array (in all caps)
 
   def initialize(title)
     @title = title
@@ -10,4 +14,14 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
-end
+  # create the writer/setter for genre and add the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre # class constant array, pushes genre into it
+  end
+end 
+# have the class keep track of all of the genres of all of the Books we create
+
+# whenever we see :: in an error for example, it indicates "name spacing". In other words, 
+# it tells us that the thing on the right side of the colons is a thing defined within the context, 
+# or name space, of the thing on the left side of the colons
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = [] # class constant array accessible by all instances
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand # push in brand into BRANDS ARRAY
+    BRANDS == BRANDS.uniq! # set BRANDS to only uniq in place BRANDS
   end
 
   def cobble
@@ -11,4 +15,11 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-end
+  
+
+  # def brand=(brand)
+  #   @brand = brand
+  #   BRANDS << brand # class constant array, pushes genre into it
+  # end
+end 
+
